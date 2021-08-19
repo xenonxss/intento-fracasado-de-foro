94e272596041a3d80112d3af204d3228acf8bbe7
NotXenon <ismaelah102@gmail.com>
2021-08-19 22:01:22 +0200

    s

--- register.php
+++ register.php
@@ -1,4 +1,5 @@
 <?php
+$c = true;
 include("database.php");
 if (isset($_GET["token"])) {
     $ahora = time();
@@ -33,55 +34,71 @@ if (isset($_POST["correo"])) {
     $clave_r = $_POST["pass_r"];
     $token = generateRandomString(50);
 
-    if ($clave == $clave_r) {
-        $clave_encriptada = password_hash($clave, PASSWORD_DEFAULT);
-        $ahora = time();
+    function checkEmail($email) {
+        $find1 = strpos($email, '@');
+        $find2 = strpos($email, '.');
+        return ($find1 !== false && $find2 !== false && $find2 > $find1);
+     }
 
-        $sql = "SELECT * FROM `users` WHERE `correo`='$correo';";
-        $do = mysqli_query($link, $sql);
-        if ($do->num_rows > 0){
-            echo 'che pibe, ya estas registrado';
-
-        }
-        else{
-            $sql = "INSERT INTO `users` (`id`, `user`, `correo`, `password`, `token`, `lastonline`) VALUES (NULL, '$correo', '$correo', '$clave_encriptada', '$token', '$ahora');";
-            if (mysqli_query($link, $sql)) {
-                if (enviarmail("Registrate en PORNHUB!", $correo, "Nah es coña, regitrate aqui <a href='http://localhost/foroxs/register.php?token=$token'>Pincha aqui :)</a>")) {
+    if(checkEmail($correo)){
+        $c = true;
+        if ($clave == $clave_r) {
+            $clave_encriptada = password_hash($clave, PASSWORD_DEFAULT);
+            $ahora = time();
+    
+            $sql = "SELECT * FROM `users` WHERE `correo`='$correo';";
+            $do = mysqli_query($link, $sql);
+            if ($do->num_rows > 0){
+                echo '<script>alert("Ya hay un usuario con este correo.")</script>';
+                
+               
+            }
+            else{
+                $sql = "INSERT INTO `users` (`id`, `user`, `correo`, `password`, `token`, `lastonline`) VALUES (NULL, '$correo', '$correo', '$clave_encriptada', '$token', '$ahora');";
+                if (mysqli_query($link, $sql)) {
+                    if (enviarmail("Registrate en PORNHUB!", $correo, "Nah es coña, regitrate aqui <a href='http://localhost/foroxs/register.php?token=$token'>Pincha aqui :)</a>")) {
+                    }
+                    echo "Usuario registrado! Tienes que verificar tu mail...";
+                    exit;
                 }
-                echo "Usuario registrado! Tienes que verificar tu mail...";
-                exit;
             }
         }
-
-        
     }
+    else{
+        echo 'el correo no es valido';
+        $c = false;
+
+    }
+
+    
 }
 ?>
 
 <head>
     <title>Bienvenido a foroxss</title>
     <link rel="stylesheet" href="./registerstyle.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;500;600&display=swap" rel="stylesheet">
 </head>
 
+<!--<body onload='document.getElementById("señala").classList.add("bu")'>-->
 <body>
-    <form action="" class="formulario" method="post" id="form">
+    <form class="formulario <?php if($c==false){echo 'error';} ?>" method="post" id="form">
         <div id="form-items">
-
             <input type="text" name="correo" placeholder="Correo" required><br><br>
             <input type="password" name="pass" placeholder="Contraseña" id="pass" required><br><br>
             <input type="password" name="pass_r" placeholder="Confirmar contraseña" id="pass_r" required><br><br>
-
         </div>
         <div id="register-boton">
             <button type="action" onclick="submit()">Registrarse</button>
         </div>
-
     </form>
+    <img id="señala" class="susto" src="./señalar.png">
 </body>
 <script>
     function submit() {
         if (document.getElementById("pass").value == document.getElementById("pass_r").value) {
-            console.log("oK");
             document.getElementById("form").submit();
         }
     }
