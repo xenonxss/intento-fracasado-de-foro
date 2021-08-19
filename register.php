56ebf130feadcbcaddf58e61172496ba4e319b82
Your Name <you@example.com>
2021-08-19 18:38:55 +0200

    bdo

--- register.php
+++ register.php
@@ -1,11 +1,26 @@
 <?php
+    if(isset($_POST["nombre"])){
+        include("database.php");
+        $nombre = $_POST["nombre"];
+        $clave = $_POST["pass"];
+        $clave_r = $_POST["pass_r"];
 
+        if($clave == $clave_r){
+            $clave_encriptada = password_hash($clave, PASSWORD_DEFAULT);
+            $ahora = time();
+            $sql = "INSERT INTO `users` (`id`, `user`, `password`, `lastonline`) VALUES (NULL, '$nombre', '$clave_encriptada', '$ahora');";
+            if(mysqli_query($link, $sql)){
+                echo "Usuario registrado!";
+                exit;
+            }
+        }
+    }
 ?>
 
 <form action="" method="post" id="form">
-    <input type="text" name="nombre"><br><br>
-    <input type="password" name="pass" id="pass"><br><br>
-    <input type="password" name="pass_r" id="pass_r"><br><br>
+    <input type="text" name="nombre" required><br><br>
+    <input type="password" name="pass" id="pass" required><br><br>
+    <input type="password" name="pass_r" id="pass_r" required><br><br>
     <button type="action" onclick="submit()">Registrarse</button>
 </form>
 
